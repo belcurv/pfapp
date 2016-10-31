596887a3ed112d6865ecc80e99610c0622dda644
Jay Schwane <jrschwane@uwalumni.com>
2016-10-31 13:08:09 -0500

    Add Commute-calc function to generate results array, Add results table to commute-calc template

--- ver_2/js/pfapp-commute-calc.ctrl.js
+++ ver_2/js/pfapp-commute-calc.ctrl.js
@@ -19,10 +19,76 @@
             vm.myObj = {
                 origin: "Milwaukee, WI 53207 USA",
                 destination: "New Berlin, WI 53146 USA",
-                mileageRate: .54,
+                mileageRate: 0.54,
+                roundTripFlag: 1,        // 1 = 1-way, 2 = round-trip
+                commuteArr: [],
                 calcCommute: calcCommute,
                 costCommute: costCommute
             };
+            
+
+            /* array structure:
+             * hours per ...
+            
+            //        |      time      |   distance     |      cost
+            // ------------------------------------------------------------
+            // day    |  minutes/60    |     miles      |   miles*rate
+            // week   |  minutes/12    |    miles*5     |  5*miles*rate
+            // month  |  minutes/3     |    miles*20    |  20*miles*rate
+            // year   |  (mins/12)*52  |   miles*260    |  260*miles*rate
+            // 5 yrs  |  (mins/12)*260 |   miles*1300   | 1300*miles*rate
+            // 10 yrs |  (mins/12)*520 |   miles*2600   | 2600*miles*rate
+            
+            */
+            function commuteArray(meters, seconds) {
+                var arr = [],
+                    rate = vm.myObj.mileageRate,      // mileage rate
+                    rtMult = vm.myObj.roundTripFlag,  // round-trip multiplier
+                    minutes = seconds / 60,
+                    miles = meters / 1609.344;
+                
+                arr = [
+                    {
+                        per: "Day",
+                        time: rtMult * minutes / 60,
+                        dist: rtMult * miles,
+                        cost: rtMult * miles * rate
+                    },
+                    {
+                        per: "Week",
+                        time: rtMult * minutes / 12,
+                        dist: rtMult * miles * 5,
+                        cost: rtMult * miles * rate * 5
+                    },
+                    {
+                        per: "Month",
+                        time: rtMult * minutes / 3,
+                        dist: rtMult * miles * 20,
+                        cost: rtMult * miles * rate * 20
+                    },
+                    {
+                        per: "Year",
+                        time: rtMult * (minutes / 12) * 52,
+                        dist: rtMult * miles * 260,
+                        cost: rtMult * miles * rate * 260
+                    },
+                    {
+                        per: "5 Years",
+                        time: rtMult * (minutes / 12) * 260,
+                        dist: rtMult * miles * 1300,
+                        cost: rtMult * miles * rate * 1300
+                    },
+                    {
+                        per: "10 Years",
+                        time: rtMult * (minutes / 12) * 520,
+                        dist: rtMult * miles * 2600,
+                        cost: rtMult * miles * rate * 2600
+                    }
+                ];
+                
+                console.log(arr);
+                return arr;
+            }
 
             function calcCommute(origin, destination) {
                 // method taks request object & callback
@@ -44,7 +110,9 @@
                     vm.myObj.distance = meters  / 1609.344;
                     vm.myObj.duration = seconds / 3600;
                     vm.myObj.responseObject = JSON.stringify(response, null, '  ');
-                    console.log(JSON.stringify(response, null, '  '));
+//                    console.log(JSON.stringify(response, null, '  '));
+                    vm.myObj.commuteArr = commuteArray(meters, seconds);
+                    
                 } else {
                     console.log("Error: " + status);
                 }
